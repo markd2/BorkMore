66f9224226cf936579d5083cd052af3aa94cb4a3
Mark Dalrymple <markd@bignerdranch.com>
2015-04-25 21:04:55 -0400

    bootstrap with new project

---
+++ BorkMore/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  BorkMore
+//
+//  Created by Mark Dalrymple on 4/25/15.
+//  Copyright (c) 2015 Borkware. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ BorkMore/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BorkMore
+//
+//  Created by Mark Dalrymple on 4/25/15.
+//  Copyright (c) 2015 Borkware. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
